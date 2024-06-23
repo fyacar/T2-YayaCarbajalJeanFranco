bdfed9b7cc7f811f4243ada162c1ba3abb48180f
Jean Y <fyacar_14@hotmail.com>
2024-06-22 21:27:41 -0500

    Pregunta 3 Primer Commit

--- src/app/preguntas/pregunta3/pregunta3.component.ts
+++ src/app/preguntas/pregunta3/pregunta3.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta3',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, CommonModule],
   templateUrl: './pregunta3.component.html',
   styleUrl: './pregunta3.component.css'
 })
 export class Pregunta3Component {
 
+  costoFinal: number = 0;
+
+  costoTotal(cantidadCursos: HTMLInputElement){
+    const cantCursos = parseInt(cantidadCursos.value)
+    if (cantCursos > 5) {
+      this.costoFinal = 520*0.1
+  } else {
+      this.costoFinal = 520;
+  } 
+  }
 }
+
