18acbcc068de12329eb2fe9be08dfd5f1fd31a57
Jean Y <fyacar_14@hotmail.com>
2024-06-22 21:38:27 -0500

    Pregunta 4 primer Commit

--- src/app/preguntas/pregunta4/pregunta4.component.ts
+++ src/app/preguntas/pregunta4/pregunta4.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta4',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, CommonModule],
   templateUrl: './pregunta4.component.html',
   styleUrl: './pregunta4.component.css'
 })
 export class Pregunta4Component {
+
+  vehiculos: string[] = ["Camión", "Autobús", "Turismo","Motocicleta"];
 
 }
