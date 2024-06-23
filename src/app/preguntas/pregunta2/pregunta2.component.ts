dad5f80a18bd1740dc490d0a1d59df7c3d9273ab
Jean Y <fyacar_14@hotmail.com>
2024-06-22 21:05:53 -0500

    Pregunta 2 primer commit

--- src/app/preguntas/pregunta2/pregunta2.component.ts
+++ src/app/preguntas/pregunta2/pregunta2.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pregunta2',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pregunta2.component.html',
   styleUrl: './pregunta2.component.css'
 })
 export class Pregunta2Component {
 
+  calcularSalario(salario: HTMLInputElement, tiempo: HTMLInputElement):void{
+    
+   }
 }
